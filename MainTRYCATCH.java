d0d2ac6bee5b25a89e94f06ea17a9185409c8533
Sudiksha <sudiksharavle2839@gmail.com>
2022-08-05 01:52:40 +0530

    git init

---
+++ MainTRYCATCH.java
@@ -0,0 +1,17 @@
+class MainTRYCATCH 
+{
+  public static void main(String[] args) 
+  {
+
+    try 
+	{
+      int divideByZero = 5 / 0;
+      System.out.println("Rest of code in try block");
+    }
+    
+    catch (ArithmeticException e) 
+	{
+      System.out.println("ArithmeticException => " + e.getMessage());
+    }
+  }
+}
---
+++ MainThrowkey.java
@@ -0,0 +1,22 @@
+import java.io.*;
+class MainThrowkey
+{
+ 
+  public static void findFile() throws IOException 
+  {
+    File newFile = new File("test.txt");
+    FileInputStream stream = new FileInputStream(newFile);
+  }
+
+  public static void main(String[] args) 
+  {
+    try 
+	{
+      findFile();
+    }
+    catch (IOException e) 
+	{
+      System.out.println(e);
+    }
+  }
+}
